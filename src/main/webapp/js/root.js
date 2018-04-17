4cd959b1ca009ae68a116d9505a93e5127fcf4b7
hxy317 <396686905@qq.com>
2018-04-17 22:05:11 +0800

    增加表单校验

--- src/main/webapp/js/root.js
+++ src/main/webapp/js/root.js
@@ -43,7 +43,7 @@ function queryTeacherParams(params) {
 	return query;
 }
 
-/*//表单提交前再进行一次验证
+//表单提交前再进行一次验证
 function classSubmitFn() {
   	var bootstrapValidator = $("#classForm").data("bootstrapValidator");
   	bootstrapValidator.validate();
@@ -130,4 +130,158 @@ function subjectValidate(){
 	        }
 	    }
 	});
-}*/
+}
+
+//表单提交前再进行一次验证
+function studentSubmitFn() {
+	var bootstrapValidator = $("#studentForm").data("bootstrapValidator");
+	bootstrapValidator.validate();
+	return bootstrapValidator.isValid();
+}
+//表单验证
+function studentValidate(){
+	$("#studentForm").bootstrapValidator({
+	    message: 'This value is not valid',
+	    feedbackIcons: {
+	        valid: 'glyphicon glyphicon-ok',
+	        invalid: 'glyphicon glyphicon-remove',
+	        validating: 'glyphicon glyphicon-refresh'
+	    },
+	    fields: {
+	    	num: {
+	            message: "学号校验失败",
+	            validators: {
+	                notEmpty: {
+	                    message: "学号不能为空"
+	                },
+	                stringLength: {//检测长度  
+                        min: 4,  
+                        max: 30,  
+                        message: '学号必须在4-30之间'  
+                    }
+	            }
+	        },
+	        password: {
+	        	message: "密码校验失败",
+	        	validators: {
+	        		notEmpty: {
+	        			message: "学号不能为空"
+	        		},
+	        		stringLength: {//检测长度  
+                        min: 4,  
+                        max: 30,  
+                        message: '密码长度必须在6-30之间'  
+                    },
+	        	}
+	        },
+	        name: {
+	            message: "姓名校验失败",
+	            validators: {
+	                notEmpty: {
+	                    message: "姓名不能为空"
+	                }
+	            }
+	        },
+	        phone: {
+	            message: "手机号码校验失败",
+	            validators: {
+	            	notEmpty: {
+	                    message: "手机号不能为空"
+	                },
+	                regexp: {//正则验证  
+                        regexp: /^[1][3,4,5,7,8][0-9]{9}$/,  
+                        message: '请输入正确的手机号码'  
+                    }, 
+	            }
+	        },
+	        mail: {
+	            message: "邮箱校验失败",
+	            validators: {
+	            	notEmpty: {
+	                    message: "邮箱不能为空"
+	                },
+	            	emailAddress: {//验证email地址  
+                        message: '请输入正确的邮箱地址'  
+                    },  
+	            }
+	        }
+	    }
+	});
+}
+
+//表单提交前再进行一次验证
+function teacherSubmitFn() {
+	var bootstrapValidator = $("#teacherForm").data("bootstrapValidator");
+	bootstrapValidator.validate();
+	return bootstrapValidator.isValid();
+}
+//表单验证
+function teacherValidate(){
+	$("#teacherForm").bootstrapValidator({
+	    message: 'This value is not valid',
+	    feedbackIcons: {
+	        valid: 'glyphicon glyphicon-ok',
+	        invalid: 'glyphicon glyphicon-remove',
+	        validating: 'glyphicon glyphicon-refresh'
+	    },
+	    fields: {
+	    	num: {
+	            message: "学号校验失败",
+	            validators: {
+	                notEmpty: {
+	                    message: "学号不能为空"
+	                },
+	                stringLength: {//检测长度  
+                        min: 4,  
+                        max: 30,  
+                        message: '学号必须在4-30之间'  
+                    }
+	            }
+	        },
+	        password: {
+	        	message: "密码校验失败",
+	        	validators: {
+	        		notEmpty: {
+	        			message: "学号不能为空"
+	        		},
+	        		stringLength: {//检测长度  
+                        min: 4,  
+                        max: 30,  
+                        message: '密码长度必须在6-30之间'  
+                    },
+	        	}
+	        },
+	        name: {
+	            message: "姓名校验失败",
+	            validators: {
+	                notEmpty: {
+	                    message: "姓名不能为空"
+	                }
+	            }
+	        },
+	        phone: {
+	            message: "手机号码校验失败",
+	            validators: {
+	            	notEmpty: {
+	                    message: "手机号不能为空"
+	                },
+	                regexp: {//正则验证  
+                        regexp: /^[1][3,4,5,7,8][0-9]{9}$/,  
+                        message: '请输入正确的手机号码'  
+                    }, 
+	            }
+	        },
+	        mail: {
+	            message: "邮箱校验失败",
+	            validators: {
+	            	notEmpty: {
+	                    message: "邮箱不能为空"
+	                },
+	            	emailAddress: {//验证email地址  
+                        message: '请输入正确的邮箱地址'  
+                    },  
+	            }
+	        }
+	    }
+	});
+}
